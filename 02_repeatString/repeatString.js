803dcedacbec81f0a99f138c0051c0ea2a7abf07
mikowesome <jm_mapilisan@yahoo.com>
2022-08-05 18:11:04 +0800

    finish exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num === 0) {
+        return "";
+    } else if (num === 1) {
+        return str
+    } else if (num < 0){
+        return 'ERROR';
+    } else if (num > 0) {
+        return str + repeatString(str, num - 1);
+    } else if (num = Math.floor(Math.random() * 1000)) {
+        return str + repeatString(str, num - 1)
+    } else if (str = '' && num > 0) {
+        return str + repeatString(str, num - 1);
+    }
 };
 
 // Do not edit below this line
